05f35c12c8952a083431368294f5a54e7e71944f
burak <dev.burakerden@gmail.com>
2023-04-30 17:41:00 +0300

    [DEV] set profile cv header

--- InstagramClone/InstagramClone/View/ProfileCell.swift
+++ InstagramClone/InstagramClone/View/ProfileCell.swift
@@ -12,13 +12,22 @@ class ProfileCell: UICollectionViewCell {
     
     //MARK: - Properties
 
-    
+    private let postImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.image = UIImage(named: "venom-7")
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        return iv
+    }()
     
     //MARK: - Life Cycle
 
     override init(frame: CGRect) {
         super.init(frame: frame)
         backgroundColor = .lightGray
+        
+        addSubview(postImageView)
+        postImageView.fillSuperview()
     }
     
     required init?(coder: NSCoder) {
--- InstagramClone/InstagramClone/View/ProfileHeader.swift
+++ InstagramClone/InstagramClone/View/ProfileHeader.swift
@@ -12,13 +12,121 @@ class ProfileHeader: UICollectionReusableView {
     
     //MARK: - Properties
 
+    private let profileImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.image = UIImage(named: "venom-7")
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        return iv
+    }()
     
+    private let nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Venom Black"
+        label.font = UIFont.boldSystemFont(ofSize: 14)
+        return label
+    }()
+    
+    private lazy var postsLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 2
+        label.textAlignment = .center
+        label.attributedText = attributedStatText(value: 5, label: "Posts")
+        return label
+    }()
+    
+    private lazy var followersLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 2
+        label.textAlignment = .center
+        label.attributedText = attributedStatText(value: 2, label: "Followers")
+        return label
+    }()
+    
+    private lazy var followingLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 2
+        label.textAlignment = .center
+        label.attributedText = attributedStatText(value: 1, label: "Following")
+        return label
+    }()
+    
+    private lazy var editProfileFollowButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Edit Profile", for: .normal)
+        button.layer.cornerRadius = 3
+        button.layer.borderColor = UIColor.lightGray.cgColor
+        button.layer.borderWidth = 0.5
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        button.setTitleColor(.black, for: .normal)
+        button.addTarget(self, action: #selector(handleprofileFollowTapped), for: .touchUpInside)
+        return button
+    }()
+    
+    private lazy var gridButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(UIImage(named: "grid"), for: .normal)
+        button.addTarget(self, action: #selector(handleprofileFollowTapped), for: .touchUpInside)
+        return button
+    }()
+    
+    private lazy var listButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(UIImage(named: "list"), for: .normal)
+        button.addTarget(self, action: #selector(handleprofileFollowTapped), for: .touchUpInside)
+        return button
+    }()
+    
+    private lazy var bookmarkButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(UIImage(named: "ribbon"), for: .normal)
+        button.addTarget(self, action: #selector(handleprofileFollowTapped), for: .touchUpInside)
+        return button
+    }()
     
     //MARK: - Life Cycle
 
     override init(frame: CGRect) {
         super.init(frame: frame)
-        backgroundColor = .systemPink
+        backgroundColor = .white
+        
+        addSubview(profileImageView)
+        profileImageView.anchor(top: topAnchor, left: leftAnchor, paddingTop: 16, paddingLeft: 12)
+        profileImageView.setDimensions(height: 80, width: 80)
+        profileImageView.layer.cornerRadius = 80 / 2
+        
+        addSubview(nameLabel)
+        nameLabel.anchor(top: profileImageView.bottomAnchor, left: leftAnchor, paddingTop: 12, paddingLeft: 12)
+        
+        addSubview(editProfileFollowButton)
+        editProfileFollowButton.anchor(top: nameLabel.bottomAnchor, left: leftAnchor, right: rightAnchor, paddingTop: 16, paddingLeft: 24, paddingRight: 24)
+        
+        let stack = UIStackView(arrangedSubviews: [postsLabel, followersLabel, followingLabel])
+        stack.distribution = .fillEqually
+        
+        addSubview(stack)
+        stack.centerY(inView: profileImageView)
+        stack.anchor(left: profileImageView.rightAnchor, right: rightAnchor, paddingLeft: 12, paddingRight: 12, height: 50)
+        
+        let topDivider = UIView()
+        topDivider.backgroundColor = .lightGray
+        
+        let bottomDivider = UIView()
+        bottomDivider.backgroundColor = .lightGray
+        
+        let buttonStack = UIStackView(arrangedSubviews: [gridButton, listButton, bookmarkButton])
+        buttonStack.distribution = .fillEqually
+        
+        addSubview(buttonStack)
+        addSubview(topDivider)
+        addSubview(bottomDivider)
+        
+        buttonStack.anchor(left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, height: 50)
+        
+        topDivider.anchor(top: buttonStack.topAnchor, left: leftAnchor, right: rightAnchor, height: 0.5)
+        
+        bottomDivider.anchor(top: buttonStack.bottomAnchor, left: leftAnchor, right: rightAnchor, height: 1)
+        
     }
     
     required init?(coder: NSCoder) {
@@ -27,6 +135,16 @@ class ProfileHeader: UICollectionReusableView {
     
     //MARK: - Actions
     
+    @objc func handleprofileFollowTapped() {
+        print("DEBUG: handleprofileFollowTapped")
+
+    }
 
     //MARK: - Helpers
+    
+    func attributedStatText(value: Int, label: String) -> NSAttributedString {
+        let attributedText = NSMutableAttributedString(string: "\(value)\n", attributes: [.font: UIFont.boldSystemFont(ofSize: 14)])
+        attributedText.append(NSAttributedString(string: label, attributes: [.font: UIFont.systemFont(ofSize: 14), .foregroundColor: UIColor.lightGray]))
+        return attributedText
+    }
 }
